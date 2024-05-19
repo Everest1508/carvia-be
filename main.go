949e92d55d56a11d4e7d3b73fded3e041517cf29
Everest1508 <riteshmahale15@gmail.com>
2024-05-19 14:43:36 +0530

    cc

--- main.go
+++ main.go
@@ -3,24 +3,6 @@ package main
 import "fmt"
 
 func main(){
-	n:=10
-	for i:=0;i<n;i++{
-		fmt.Println(space(n-1-i)+star(i+1))
-	}
-}
-func space(j int) string {
-	space_str := ""
-	for i:=0;i<j;i++{
-		space_str+=" "
-	}
-	return space_str
-}
-
-func star(j int) string {
-	star_str := ""
-	for i:=0;i<j;i++{
-		star_str+="* "
-	}
-	return star_str
+	fmt.Println("heyy")
 }
 
